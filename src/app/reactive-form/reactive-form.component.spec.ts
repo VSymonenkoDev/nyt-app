bfe344659732bf95d3ad5dbb3a7cd7983b2bec17
vsimonenko <v.simonenko@mobilunity.com>
2019-09-10 18:16:02 +0300

    form tests

--- src/app/reactive-form/reactive-form.component.spec.ts
+++ src/app/reactive-form/reactive-form.component.spec.ts
@@ -1,6 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ReactiveFormComponent } from './reactive-form.component';
+import {ReactiveFormComponent} from './reactive-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 describe('ReactiveFormComponent', () => {
   let component: ReactiveFormComponent;
@@ -8,7 +9,8 @@ describe('ReactiveFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReactiveFormComponent ]
+      declarations: [ ReactiveFormComponent ],
+      imports: [ReactiveFormsModule]
     })
     .compileComponents();
   }));
@@ -21,5 +23,24 @@ describe('ReactiveFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should support preset data', () => {
+    const user = {
+      userName: 'testUser',
+      email: 'testUser@gmail.com',
+      subscribe: true
+    };
+    component.registrationForm.patchValue(user);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#name').value).toEqual(user.userName);
+    expect(compiled.querySelector('#email').value).toEqual(user.email);
+    expect(compiled.querySelector('#subscribe').value).toBeTruthy();
+  });
+
+  it('shouldn\'t pass forbidden user name', () => {
+    const user = {userName: 'password'};
+    component.registrationForm.patchValue(user);
+    expect(component.registrationForm.valid).toBeFalsy();
   });
 });
